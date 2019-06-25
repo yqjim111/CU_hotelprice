68f47bc2db7f417534e8ec580e0692e8bb2ec416
gg2691 <gg2691@columbia.edu>
2019-06-24 22:49:29 -0400

    1

---
+++ map.js
@@ -0,0 +1,14 @@
+'use strict'        // let the browser know we're serious
+
+// debug statement letting us know the file is loaded
+console.log('Loaded map.js')
+
+// your mapbox token
+mapboxgl.accessToken = 'pk.eyJ1IjoieXFqaW0xMTEiLCJhIjoiY2psb2k5ZGZkMXR1czNxdDV3dW55b3hyaiJ9.ihJM3eJ8hNrDmr7EYKjo7w'
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/yqjim111/cjuoazzbsbe001fn0v71i4psi',
+    center: [-66.390, 18.187],
+    zoom: 7.8
+})
